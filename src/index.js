ed28f375f32de17b347093374df6046be3cc991d
Evgenii Shevchenko <evgenii@Evgeniis-MacBook-Air.local>
2021-09-26 01:12:57 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { promises as fs } from 'fs';
+import path from 'path';
 import axios from 'axios';
 import urlParser from './urlParser';
 
-export default async (url, dirPath) => {
+export default async (url, outputDirPath = process.cwd()) => {
   const parsedUrl = urlParser(url);
   const filename = `${parsedUrl}.html`;
   const response = await axios.get(url);
   const html = response.data;
-  const filepath = `${dirPath}/${filename}`;
+  const filepath = path.join(outputDirPath, filename);
   await fs.writeFile(filepath, html);
 
-  return Promise.resolve({ filepath: `${dirPath}/${filename}` });
+  return Promise.resolve({ filepath });
 };

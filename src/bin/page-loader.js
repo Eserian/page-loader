65bd3f5b9930208811dc5d94307cb27a26b6a245
Evgenii Shevchenko <evgenii@Evgeniis-MacBook-Air.local>
2021-10-10 21:08:10 +0300

    fix

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -2,14 +2,14 @@
 
 import { Command } from 'commander';
 import { version } from '../../package.json';
-import pageLoader from '../index';
+import pageLoader from '../index.js';
 
 const program = new Command();
 
 program
   .version(version)
   .arguments('<url>')
-  .option('-O, --output <dir>', 'output dir', process.cwd())
+  .option('-o, --output <dir>', 'output dir', process.cwd())
   .action(async (url, options) => {
     try {
       const { filepath } = await pageLoader(url, options.output);
